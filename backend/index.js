54a1f0158a71712aca7828b9a2844d78d65225e5
SPARSHAV2003 <sparshav2003@gmail.com>
2023-09-18 17:59:50 +0530

    Files added

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const route = require("./routes/route");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3500;
+
+// CORS configuration
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000", // Replace with your frontend's URL
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Add the HTTP methods you need
+//   })
+// );
+app.use(cors())
+// Parse JSON request bodies
+app.use(express.json());
+
+// Mount your routes
+app.use("/api2", route);
+app.use("/api", require("./routes/Create_user.js"));
+
+// DB Connection
+mongoose
+  .connect("mongodb+srv://SparshaV:golgappa@cluster0.cuhuq1i.mongodb.net/")
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.error("DB Connection Error:", err);
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
